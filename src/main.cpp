270663448db0b5e5b0c3e72f026f0cb193033b47
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-11-06 15:24:11 -0600

    It works! Formatting works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,21 @@
+#include <fmt/core.h>
+
 #include <array>
 #include <iostream>
 #include <numeric>
 #include <ranges>
 #include <span>
 
-#include <fmt/core.h>
-
 #include "add.hpp"
 #include "bin_search.hpp"
-
 
 constexpr auto sum_all(const std::ranges::range auto& container) {
     return std::accumulate(container.begin(), container.end(), 0);
 }
 
-
-template <std::size_t N>
+template<std::size_t N>
 consteval void test_bin_search() {
-    constexpr std::array arr{0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    constexpr std::array arr {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     constexpr auto index = binary_search(arr, static_cast<int>(N)).value();
     static_assert(index == N, "God why");
@@ -35,13 +33,11 @@ consteval void test_all_bin_search() {
     test_bin_search<9>();
 }
 
-
 int main() {
     test_all_bin_search();
 
-    constexpr std::array arr{0, 1, 2, 3, 4, 5 };
+    constexpr std::array arr {0, 1, 2, 3, 4, 5};
     constexpr auto index = binary_search(arr, 69);
 
     fmt::println("Index is {}", index.value_or(69));
 }
-
