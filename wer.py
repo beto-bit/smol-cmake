cdc7a4e4432a9116e6d6014a459133c94940d34a
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-11-10 10:34:18 -0600

    I think this is done

--- wer.py
+++ wer.py
@@ -20,8 +20,11 @@ class WerConfig:
         
         self.config = self.cached_config
         self.build_config: dict[str, Any] = self.config["build"]
-        self.platform_config = get_platform_config(self.config)
+        self.build_dir: str = self.config["build"]["dir"]
+        self.platform_config = WerConfig.get_platform_config(self.config)
         self.format_config: dict[str, Any] = self.config.get("format")
+
+        self.vcpkg_enable: str | None = self.config.get("enable")
     
     def load_config() -> dict[str, Any]:
         with open("wer.toml", "rb") as f:
@@ -84,55 +87,6 @@ class WerConfig:
                 return set(excluded_files)
     
 
-# Configuration utilities
-def get_config() -> dict[str, Any]:
-    with open("wer.toml", "rb") as f:
-        return tomllib.load(f)
-
-def get_platform_config(config) -> dict[str, Any] | None:
-    if platform.system() == "Windows":
-        return config.get("windows")
-    
-    return config.get("unix")
-
-def get_generator(config) -> str | None:
-    if platform_config := get_platform_config(config):
-        return platform_config.get("generator")
-
-    return config["build"].get("generator")
-
-
-def get_c_compiler(config) -> str | None:
-    if platform_config := get_platform_config(config):
-        return platform_config.get("cc")
-
-def get_cxx_compiler(config) -> str | None:
-    if platform_config := get_platform_config(config):
-        return platform_config.get("cxx")
-
-def get_format_style(config) -> str | None:
-    if format_config := config.get("format"):
-        return format_config.get("style")
-
-def get_excluded_format_files(config) -> Set[str]:
-    if format_config := config.get("format"):
-        if excluded_files := format_config.get("exclude"):
-            return set(excluded_files)
-
-    return set()
-
-def get_format_files(config) -> Set[str]:
-    recursive_glob = lambda x: glob.glob(x, recursive=True)
-
-    if not (format_config := config.get("format")):
-        return set()
-
-    format_glob = format_config.get("glob")
-    format_files = map(recursive_glob, format_glob)
-
-    # Dedup and remove excluded files
-    return set(chain.from_iterable(format_files)) - get_excluded_format_files(config)
-
 def get_vcpkg_bootstrap() -> str:
     if platform.system() == "Windows":
         return ".\vcpkg\bootstrap-vcpkg.bat"
@@ -159,52 +113,16 @@ def create_env_vars(config: WerConfig, create_ccs: bool) -> dict[str, str]:
     
     return env_vars
 
-def create_env_vars(config, create_ccs: bool) -> dict[str, str]:
-    env_vars = {
-        **os.environ,
-        "CMAKE_EXPORT_COMPILE_COMMANDS": str(int(create_ccs))
-    }
-
-    if c_compiler := get_c_compiler(config):
-        env_vars["CC"] = c_compiler
-
-    if cpp_compiler := get_cxx_compiler(config):
-        env_vars["CXX"] = cpp_compiler
-
-    return env_vars
-
 def create_cmake_commands(
     config: WerConfig,
-    build_dir: str,
-    enable_vcpkg: bool,
     vcpkg_dir: str
 ) -> List[str]:
-    commands = ["cmake", "-S", ".", "-B", build_dir]
+    commands = ["cmake", "-S", ".", "-B", config.build_dir]
 
     if config.generator:
         commands.append(f"-G {config.generator}")
 
-    if enable_vcpkg:
-        if os.path.exists(vcpkg_dir):
-            commands.append(f"-DCMAKE_TOOLCHAIN_FILE={vcpkg_dir}/scripts/buildsystems/vcpkg.cmake")
-        else:
-            click.echo("Please first run `./wer.py vcpkg setup` or disable vcpkg on `wer.toml`")
-            raise FileNotFoundError
-
-    return commands
-
-def create_cmake_commands(
-        build_dir: str,
-        generator: str | None,
-        enable_vcpkg: bool,
-        vcpkg_dir: str
-) -> List[str]:
-    commands = ["cmake", "-S", ".", "-B", build_dir]
-
-    if generator:
-        commands.append(f"-G {generator}")
-
-    if enable_vcpkg:
+    if config.enable_vcpkg:
         if os.path.exists(vcpkg_dir):
             commands.append(f"-DCMAKE_TOOLCHAIN_FILE={vcpkg_dir}/scripts/buildsystems/vcpkg.cmake")
         else:
@@ -225,9 +143,9 @@ def cli():
 def build():
     """Builds everything"""
 
-    if os.path.exists(BUILD_DIR):
+    if os.path.exists(WER_CONFIG.build_dir):
         click.echo("Building...")
-        subprocess.run(["cmake", "--build", BUILD_DIR])
+        subprocess.run(["cmake", "--build", WER_CONFIG.build_config])
     else:
         click.echo("Please first run `./wer.py configure`")
 
@@ -239,7 +157,7 @@ def clean():
     click.echo("Cleaning everything...")
 
     try:
-        shutil.rmtree(BUILD_DIR)
+        shutil.rmtree(WER_CONFIG.build_dir)
     except FileNotFoundError:
         click.echo("Already cleaned up!")
 
@@ -249,7 +167,7 @@ def clean():
 @click.option("--list", is_flag=True, help="Lists which files are going to be formatted")
 def format(dry_run, list):
     """Formats source files. Defaults to LLVM style."""
-    if format_files := sorted(get_format_files(CONFIG)):
+    if format_files := sorted(WER_CONFIG.fmt_files):
         if list:
             click.echo('\n'.join(format_files))
             return
@@ -259,7 +177,7 @@ def format(dry_run, list):
         if not dry_run:
             commands.append("-i")
 
-        if format_style := get_format_style(CONFIG):
+        if format_style := WER_CONFIG.fmt_style:
             commands.append(f"-style={format_style}")
 
         subprocess.run(commands)
@@ -276,11 +194,8 @@ def configure(create_ccs):
     click.echo("Configuring the project...")
 
     # There should be a better way to do this
-    env_vars = create_env_vars(CONFIG, create_ccs)
-    commands = create_cmake_commands(BUILD_DIR,
-                                     GENERATOR,
-                                     VCPKG_ENABLE,
-                                     VCPKG_DIR)
+    env_vars = create_env_vars(WER_CONFIG)
+    commands = create_cmake_commands(WER_CONFIG, VCPKG_DIR)
 
     subprocess.run(commands, env=env_vars)
 
@@ -332,12 +247,7 @@ def uninstall():
         click.echo("vcpkg_installed already removed!")
 
 
-# Global variables
-CONFIG = get_config()
-BUILD_DIR = CONFIG["build"]["dir"]
-GENERATOR = get_generator(CONFIG)
-
-VCPKG_ENABLE = CONFIG["vcpkg"]["enable"]
+WER_CONFIG = WerConfig()
 
 VCPKG_REPO = "https://github.com/Microsoft/vcpkg.git"
 VCPKG_DIR = "vcpkg"
@@ -347,7 +257,4 @@ VCPKG_EXE = get_vcpkg_exe()
 
 
 if __name__ == "__main__":
-    config = WerConfig()
-    print(config.config)
-
-    # cli()
+    cli()
